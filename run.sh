3c1848d659001e5ddd16e5d5ba1fdf477157801b
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2024-12-04 17:11:31 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 start_time=$(date +%s)
+
 ## activate the virtual environment
 . env/bin/activate
+
 PROGRAM=$1
 TEST_ARG=pytest
 LINT_ARG=pylint
@@ -11,6 +13,7 @@ logs="$logs_dirname"/"$PROGRAM"_logs.log
 : >$logs
 line="\n\n"
 current_date_time=$(date "+%Y-%m-%d %H:%M:%S")
+
 echo "Started..."
 echo "Storing real-time logs in "$logs""
 echo "[ $current_date_time ] PROGRAM: $PROGRAM $PROGRAM_ARG"
@@ -23,10 +26,14 @@ elif [ $PROGRAM = $LINT_ARG ]; then
 else
     python -u $PROGRAM.py $PROGRAM_ARG >>$logs 2>&1
 fi
+
 end_time=$(date +%s)
 total_time=$(($end_time - $start_time))
+
 echo -en "\nTotal Time taken: $total_time seconds\n" >>$logs 2>&1
 echo "[ $current_date_time ] $PROGRAM $PROGRAM_ARG1 $PROGRAM_ARG2: Execution Completed." >>$logs 2>&1
+
 echo -en $line >>$logs 2>&1
+
 echo $PROGRAM $PROGRAM_ARG was executed within $total_time seconds.
 echo
