5a1c7a1b3b03906fea89bf9522dfa2ea634df3d2
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2024-12-10 17:22:26 +0530

    Update run.sh with unlimited args for python

--- run.sh
+++ run.sh
@@ -19,8 +19,10 @@ current_date_time=$(date "+%Y-%m-%d %H:%M:%S")
 
 echo "Started..."
 echo "Storing real-time logs in "$logs""
-echo "[ $current_date_time ] PROGRAM: $PROGRAM $PROGRAM_ARG"
-echo "[ $current_date_time ] PROGRAM: $PROGRAM $PROGRAM_ARG" >>$logs 2>&1
+echo "[ $current_date_time ] PROGRAM: '$PROGRAM ${@:2}'"
+echo "[ $current_date_time ] PROGRAM: '$PROGRAM ${@:2}'" >>$logs 2>&1
+echo -en "\n" >>$logs 2>&1
+
 if [ $PROGRAM = $TEST_ARG ]; then
     coverage run -m $PROGRAM $PROGRAM_ARG >>$logs 2>&1
     coverage html -d pytest_report
@@ -34,9 +36,9 @@ end_time=$(date +%s)
 total_time=$(($end_time - $start_time))
 
 echo -en "\nTotal Time taken: $total_time seconds\n" >>$logs 2>&1
-echo "[ $current_date_time ] $PROGRAM $PROGRAM_ARG1 $PROGRAM_ARG2: Execution Completed." >>$logs 2>&1
+echo "[ $current_date_time ] $PROGRAM ${@:2}: Execution Completed." >>$logs 2>&1
 
 echo -en $line >>$logs 2>&1
 
-echo $PROGRAM $PROGRAM_ARG was executed within $total_time seconds.
+echo "'$PROGRAM ${@:2}'" was executed within $total_time seconds.
 echo
