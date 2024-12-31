51124a689ae449965407edc6b4f3d8a2675e3b4d
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2024-12-31 15:27:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-from src.utils import resource_usage
-from dotenv import load_dotenv
+from src.utils.annotations import resource_usage
+from src.utils import AUTH_TOKEN
 
-import os
-
-load_dotenv(override=True)
-
-AUTH_TOKEN = os.getenv("AUTH_TOKEN")
 
 @resource_usage
-def main(n: int = 5):
+def main():
+    return None
+
+
+@resource_usage
+def get_factorial(n: int, auth_token: str):
+    if auth_token == AUTH_TOKEN:
+        print("AUTH_TOKEN Verified!")
+    else:
+        print("Wrong AUTH_TOKEN Entered..")
+
     if n < 0:
         raise ValueError("n must be a non-negative integer")
     elif n == 0 or n == 1:
         return 1
     else:
-        return n * main(n - 1)
+        return n * get_factorial(n - 1, auth_token=auth_token)
 
 
 if __name__ == "__main__":
-    result = main()
+    result = get_factorial(n=5, auth_token="token")
     print(f"RESULT: {result}")
