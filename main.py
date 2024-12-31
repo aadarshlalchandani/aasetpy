d9d6bc59bba13cdce311d351e937a893833db35f
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2024-12-31 15:39:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,27 @@ from src.utils.annotations import resource_usage
 
 @resource_usage
 def main():
-    return None
+    return get_factorial(n=5, auth_token="token")
 
 
-@resource_usage
 def get_factorial(n: int, auth_token: str):
-    if auth_token == AUTH_TOKEN:
-        print("AUTH_TOKEN Verified!")
-    else:
-        print("Wrong AUTH_TOKEN Entered..")
 
     if n < 0:
         raise ValueError("n must be a non-negative integer")
+
     elif n == 0 or n == 1:
+        if auth_token == AUTH_TOKEN:
+            print("AUTH_TOKEN Verified!")
+
+        else:
+            print("Wrong AUTH_TOKEN Entered..")
+
         return 1
+
     else:
         return n * get_factorial(n - 1, auth_token=auth_token)
 
 
 if __name__ == "__main__":
-    result = get_factorial(n=5, auth_token="token")
+    result = main()
     print(f"RESULT: {result}")
