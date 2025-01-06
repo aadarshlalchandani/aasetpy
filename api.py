37a826e6dd9956ef34bf22aeb30a5464e3880f64
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2025-01-06 17:24:19 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    from src.rest_api import API_HOST, API_PORT, uvicorn
+    from src.rest_api.api import app
+    from src.rest_api.api_logging_config import get_logging_config
+
+    uvicorn.run(
+        app=app,
+        port=API_PORT,
+        host=API_HOST,
+        log_config=get_logging_config(),
+    )
