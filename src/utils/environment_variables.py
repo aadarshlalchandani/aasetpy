7ea85c14ecee55b8d50cb7ddff5b5814fe6815f7
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2025-01-11 12:55:54 +0530

    Create environment_variables.py in utils

---
+++ src/utils/environment_variables.py
@@ -0,0 +1,21 @@
+from src.utils import BaseSettings, lru_cache
+
+
+class ENVSettings(BaseSettings):
+    class Config:
+        env_file = ".env"
+        extra = "allow"
+
+
+env_settings = ENVSettings()
+
+SPLIT_DELIMITER = ","
+
+
+class EnvironmentVariables:
+    @lru_cache
+    def __getattr__(self, env_var_name):
+        return getattr(env_settings, env_var_name.lower(), None)
+
+
+env = EnvironmentVariables()
