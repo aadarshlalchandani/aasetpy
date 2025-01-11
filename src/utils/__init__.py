b9d100116cc1c1d769c88ad3dbf1485a21b3e8ed
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2025-01-11 12:55:07 +0530

    Update utils __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,28 +4,9 @@
 import os
 import time
 import typing as t
-from functools import wraps
+from functools import lru_cache, wraps
 from typing import List, Optional, Union
 
 import psutil
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
-
-
-class Settings(BaseSettings):
-    class Config:
-        env_file = ".env"
-        extra = "allow"
-
-
-settings = Settings()
-
-SPLIT_DELIMITER = ","
-
-
-class EnvironmentVariables:
-    def __getattr__(self, env_var_name):
-        return getattr(settings, env_var_name.lower(), None)
-
-
-env = EnvironmentVariables()
