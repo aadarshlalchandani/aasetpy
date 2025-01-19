36a76aaf7529932f25096ead3e605cbef9bdf016
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2025-01-19 18:13:08 +0530

    fix run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ start_time=$(date +%s)
 PROGRAM=$1
 TEST_ARG=pytest
 LINT_ARG=pylint
-PROGRAM_ARGS=$(echo "$2" | sed 's/= / /g')
+PROGRAM_ARG=$(echo "$2" | sed 's/= / /g')
 logs_dirname=logs
 logs="$logs_dirname"/"$PROGRAM"_logs.log
 : >$logs
