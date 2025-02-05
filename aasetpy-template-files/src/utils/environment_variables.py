1fc8fa81499c143d406c2e58c3b35a1e6aba7c65
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2025-02-05 12:44:25 +0530

    Update environment_variables.py

--- aasetpy-template-files/src/utils/environment_variables.py
+++ aasetpy-template-files/src/utils/environment_variables.py
@@ -1,4 +1,5 @@
 from src.utils import BaseSettings, lru_cache
+from typing import Any
 
 
 class ENVSettings(BaseSettings):
@@ -14,6 +15,10 @@ class EnvironmentVariables:
     @lru_cache
     def __getattr__(self, env_var_name):
         return getattr(env_settings, env_var_name.lower(), None)
+    
+    def __call__(self, env_var_name: str, default_value: Any = None) -> Any:
+        value = getattr(env_settings, env_var_name.lower(), None)
+        return value if value is not None else default_value
 
 
 env = EnvironmentVariables()
