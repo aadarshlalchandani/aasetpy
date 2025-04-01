17a3b6b800a3186ab1e648d9f9af3115a7add911
aadarshlalchandani <aadarshlalchandani@gmail.com>
2025-04-01 19:51:51 +0530

    Add GPU monitoring support; `GPU mem is not working yet`

--- aasetpy-template-files/src/utils/annotations.py
+++ aasetpy-template-files/src/utils/annotations.py
@@ -1,6 +1,18 @@
 ## Modify 'src/utils' as per your project
 ## credits: aadarshlalchandani/aasetpy
 
+from pynvml import (
+    NVMLError,
+    nvmlDeviceGetComputeRunningProcesses,
+    nvmlDeviceGetHandleByIndex,
+    nvmlDeviceGetMemoryInfo,
+    nvmlDeviceGetName,
+    nvmlDeviceGetUtilizationRates,
+    nvmlInit,
+    nvmlShutdown,
+    nvmlDeviceGetCount,
+)
+
 from src.utils import inspect, json, os, psutil, threading, time, wraps
 
 
@@ -22,19 +34,58 @@ def get_program_pid(script_path: str, filename: str):
 def get_program_usage(pid: int, interval: float = 1.0):
     cpu_usage = []
     ram_usage = []
+    gpu_utilization = []
+    gpu_memory_usage = []
+    gpu_name = []
+
     stop_monitoring = threading.Event()
 
     def monitor():
         process = psutil.Process(pid)
+
+        # Initialize NVML
+        try:
+            nvmlInit()
+            gpu_count = nvmlDeviceGetCount()
+            print(
+                f"{gpu_count} NVIDIA GPU(s) found. Will monitor the usage of the first GPU.\n"
+            )
+            handle = nvmlDeviceGetHandleByIndex(0)
+        except NVMLError as e:
+            print(f"NVML Init failed: {e}")
+            return
+
         while not stop_monitoring.is_set():
             try:
+                # CPU and RAM
                 cpu_usage.append(process.cpu_percent(interval=interval))
                 ram_usage.append(process.memory_percent())
-            except KeyboardInterrupt:
+
+                # GPU Util and Memory
+                try:
+                    processes = nvmlDeviceGetComputeRunningProcesses(handle)
+                    for p in processes:
+                        if p.pid == pid:
+                            mem = nvmlDeviceGetMemoryInfo(handle)
+                            gpu_memory_usage.append(mem.used)
+                    util = nvmlDeviceGetUtilizationRates(handle)
+                    gpu_utilization.append(util.gpu)
+                    gpu_name_value = nvmlDeviceGetName(handle).decode("utf-8")
+                    (
+                        gpu_name.append(gpu_name_value)
+                        if gpu_name_value not in gpu_name
+                        else None
+                    )
+                except NVMLError:
+                    print("nvml error get compute running processes")
+                    pass
+
+            except (psutil.NoSuchProcess, KeyboardInterrupt):
                 break
-            except psutil.NoSuchProcess:
-                break
+
             time.sleep(interval)
+
+        nvmlShutdown()
 
     monitor_thread = threading.Thread(target=monitor)
     monitor_thread.start()
@@ -43,24 +94,55 @@ def get_program_usage(pid: int, interval: float = 1.0):
         stop_monitoring.set()
         monitor_thread.join()
 
-    return stop_monitoring_func, cpu_usage, ram_usage
+    return (
+        stop_monitoring_func,
+        cpu_usage,
+        ram_usage,
+        gpu_utilization,
+        gpu_memory_usage,
+        gpu_name,
+    )
 
 
-def get_usage_results(start_time, func_name, cpu_usage, ram_usage):
+def get_usage_results(
+    start_time,
+    func_name,
+    cpu_usage,
+    ram_usage,
+    gpu_utilization,
+    gpu_memory_usage,
+    gpu_name,
+):
     end_time = time.time()
     time_spent = end_time - start_time
     usage_results = {
         "func_name": func_name,
         "time_spent": time_spent,
     }
+
     if cpu_usage and ram_usage:
         usage_results["avg_cpu"] = sum(cpu_usage) / len(cpu_usage)
         usage_results["avg_ram"] = sum(ram_usage) / len(ram_usage)
         usage_results["max_cpu"] = max(cpu_usage)
         usage_results["max_ram"] = max(ram_usage)
 
+        usage_results["gpu_name"] = gpu_name
+        if gpu_utilization:
+            usage_results["avg_gpu_util"] = sum(gpu_utilization) / len(gpu_utilization)
+            usage_results["max_gpu_util"] = max(gpu_utilization)
+        else:
+            usage_results["avg_gpu_util"] = 0
+            usage_results["max_gpu_util"] = 0
+
+        if gpu_memory_usage:
+            usage_results["avg_gpu_mem"] = sum(gpu_memory_usage) / len(gpu_memory_usage)
+            usage_results["max_gpu_mem"] = max(gpu_memory_usage)
+        else:
+            usage_results["avg_gpu_mem"] = 0
+            usage_results["max_gpu_mem"] = 0
+
     else:
-        usage_results["error"] = "Function did not run for enough time to collect data."
+        usage_results["error"] = "Function did not run long enough to collect data."
 
     return usage_results
 
@@ -73,7 +155,15 @@ def monitor_usage(func):
             script_path=filepath,
             filename=os.path.basename(filepath),
         )
-        stop_monitoring, cpu_usage, ram_usage = get_program_usage(pid)
+
+        (
+            stop_monitoring,
+            cpu_usage,
+            ram_usage,
+            gpu_utilization,
+            gpu_memory_usage,
+            gpu_name,
+        ) = get_program_usage(pid)
 
         start_time = time.time()
         try:
@@ -86,6 +176,9 @@ def monitor_usage(func):
                 func_name=func.__name__,
                 cpu_usage=cpu_usage,
                 ram_usage=ram_usage,
+                gpu_utilization=gpu_utilization,
+                gpu_memory_usage=gpu_memory_usage,
+                gpu_name=gpu_name[0] if len(gpu_name) == 1 else gpu_name,
             )
             print()
             print("***Usage Stats***")
