a7bdc0e7e2f3d0402841cd519252fd8ac66da724
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2025-10-30 22:26:54 +0530

    Add get_batches function for batching elements

--- aasetpy-template-files/src/utils/__init__.py
+++ aasetpy-template-files/src/utils/__init__.py
@@ -25,3 +25,12 @@ SPLIT_DELIMITER = ","
 
 def text_splitter(text: str, split_delimiter: str = SPLIT_DELIMITER):
     return text.split(split_delimiter)
+
+
+def get_batches(list_of_elements: list, batch_size: int = 150):
+    batches = []
+    for idx in range(0, len(list_of_elements), batch_size):
+        batch = list_of_elements[idx : idx + batch_size]
+        batches.append(batch)
+
+    return batches
