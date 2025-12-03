bd3e9a270e35d58e68af39afb3f924f066f3cd7f
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2025-12-03 12:42:29 +0530

    Enhance utils with logging and current time functions
    Added utility functions for logging and time formatting.

--- aasetpy-template-files/src/utils/__init__.py
+++ aasetpy-template-files/src/utils/__init__.py
@@ -2,17 +2,19 @@
 ## credits: aadarshlalchandani/aasetpy
 
 import asyncio
-import datetime
 import inspect
 import json
 import os
 import re
 import threading
 import time
+import traceback
+import warnings
+from datetime import date, timedelta
 from datetime import datetime as dtime
-from datetime import timedelta
 from functools import lru_cache, wraps
-from typing import Any, Dict, List, Optional, Union
+from time import perf_counter
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import psutil
 from pydantic import BaseModel, Field
@@ -20,6 +22,10 @@ from pydantic_settings import BaseSettings
 
 ENCODE_ALGO = "utf8"
 SPLIT_DELIMITER = "^"
+
+
+def current_time():
+    return dtime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 def text_splitter(text: str, split_delimiter: str = SPLIT_DELIMITER):
@@ -42,3 +48,24 @@ def preprocess_data(data: Union[List, Dict, Any]) -> Union[List, Dict, Any]:
         return [preprocess_data(ele) for ele in data if ele]
     else:
         return data if data else None
+
+
+def print_centered(statement: str, border_char: str = "-", border_char_count: int = 50):
+    print(statement.center(border_char_count, border_char))
+
+
+def log_error(*args: str, mininmum_length: int = 15, maximum_length: int = 170):
+    """Log Errors
+
+    Args:
+        mininmum_length (int, optional): Minimum Border Length. Defaults to 15.
+        maximum_length (int, optional): Maximum Border Length. Defaults to 170.
+    """
+    max_length = max(
+        min(max([len(str(stmt)) for stmt in args]), maximum_length), mininmum_length
+    )
+    print_centered(" ERROR ", border_char="✖", border_char_count=max_length)
+    for stmt in args:
+        print(stmt)
+    print_centered(" ERROR ", border_char="✖", border_char_count=max_length)
+    return
