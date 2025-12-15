863c4a0604238a6dbe5f131347677601fe069ae8
Aadarsh Lalchandani <139241329+aadarshlalchandani@users.noreply.github.com>
2025-12-15 16:57:21 +0530

    Import wraps from src.utils in `src/server_cache/__init__.py`

--- aasetpy-template-files/src/server_cache/__init__.py
+++ aasetpy-template-files/src/server_cache/__init__.py
@@ -1,11 +1,10 @@
 ## Modify 'src/server_cache' as per your project
 ## credits: aadarshlalchandani/aasetpy
 
-from functools import wraps
 from inspect import iscoroutinefunction, signature
 
 from src.rest_api import BaseModel, Request
-from src.utils import json, time
+from src.utils import json, time, wraps
 from src.utils.environment_variables import env
 
 DEFAULT_CACHE_EXPIRE_SECONDS = float(env("DEFAULT_CACHE_EXPIRE_SECONDS", "86400"))
